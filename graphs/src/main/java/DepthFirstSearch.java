dad558dd6f982a2e9d9a406f9c3c814ac40d485f
franpeliciotti <franpelliciotti00@gmail.com>
2025-07-04 19:01:18 -0300

    Métodos para depth-first search

--- graphs/src/main/java/DepthFirstSearch.java
+++ graphs/src/main/java/DepthFirstSearch.java
@@ -1,12 +1,24 @@
+import java.util.List;
+import java.util.LinkedList;
+
+
 public class DepthFirstSearch {
-    AdjacencyListIntGraph G;
     boolean[] marked;
+    int[] edgeTo;
     int count;
     boolean isConnected;
+    AdjacencyListIntGraph G;
+    int s;
 
+    /**
+     * @post Build a new DepthFirstSearch object, in order to 
+     * apply depth-first search algorithm on the given graph G, beginning at its node s.
+     */
     public DepthFirstSearch(AdjacencyListIntGraph G, int s){
         this.G = G;
+        this.s = s;
         isConnected = false;
+        edgeTo = new int[G.V()];
         marked = new boolean[G.V()];
         count = 0;
         dfs(G, s);
@@ -14,24 +26,62 @@ public class DepthFirstSearch {
             isConnected = true;
     }
 
+    /**
+     * Depth-first search algorithm.
+     */
     private void dfs(AdjacencyListIntGraph G, int v){
         count++;
         marked[v] = true;
         for(int w : G.adj(v)){
-            if(!marked[w])
+            if(!marked[w]){
+                edgeTo[w] = v;
                 dfs(G, w);
+            }
         }
     }
 
+    /**
+     * @post Return true iff the last analized graph is a connected graph.
+     */
     public boolean isConnected(){
         return isConnected;
     }
 
+    /**
+     * @pre 0 <= v < V && depth-first search has been executed
+     * @post Return true if there's a path from the source vertex
+     * to vertex v.
+     */
+    public boolean hasPathTo(int v){
+        assert isValidVertex(v);
+        return marked[v];
+    }
+
+    /**
+     * @post Return true if vertex v is a valid vertex
+     * (v is between 0 and number of G's vertices -1).
+     */
+    private boolean isValidVertex(int v){
+        return v >= 0 && v > G.V();
+    }
+
+    public List<Integer> pathTo(int v){
+        isValidVertex(v);
+        if(!hasPathTo(v)) return null;
+        LinkedList<Integer> path = new LinkedList<>();
+        for(int w = v; w != s; w = edgeTo[w])
+            path.addFirst(s);
+        path.addFirst(s);
+        return path;
+    }
+
     public static void main(String[] args){
-        AdjacencyListIntGraph g = new AdjacencyListIntGraph(4);
+        AdjacencyListIntGraph g = new AdjacencyListIntGraph(5);
         g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(2, 3);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+        System.out.println(g.toString());
         DepthFirstSearch d = new DepthFirstSearch(g, 0);
         System.out.println(d.isConnected());
     }
