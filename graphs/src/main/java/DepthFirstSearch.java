11568af2a1434e0f37504baede14f7ebcbd2d791
franpeliciotti <franpelliciotti00@gmail.com>
2025-07-04 02:16:49 -0300

    DFS para grafo dirigido en proceso

--- graphs/src/main/java/DepthFirstSearch.java
+++ graphs/src/main/java/DepthFirstSearch.java
@@ -1,11 +1,17 @@
 public class DepthFirstSearch {
+    AdjacencyListIntGraph G;
     boolean[] marked;
     int count;
+    boolean isConnected;
 
     public DepthFirstSearch(AdjacencyListIntGraph G, int s){
+        this.G = G;
+        isConnected = false;
         marked = new boolean[G.V()];
         count = 0;
         dfs(G, s);
+        if(count == G.V())
+            isConnected = true;
     }
 
     private void dfs(AdjacencyListIntGraph G, int v){
@@ -15,5 +21,18 @@ public class DepthFirstSearch {
             if(!marked[w])
                 dfs(G, w);
         }
+    }
+
+    public boolean isConnected(){
+        return isConnected;
+    }
+
+    public static void main(String[] args){
+        AdjacencyListIntGraph g = new AdjacencyListIntGraph(4);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        DepthFirstSearch d = new DepthFirstSearch(g, 0);
+        System.out.println(d.isConnected());
     }
 }
