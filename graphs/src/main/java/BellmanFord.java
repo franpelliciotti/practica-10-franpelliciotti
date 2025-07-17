34b4b78c0a7edc31f72483f9ec8e30fe7563c3cb
franpeliciotti <franpelliciotti00@gmail.com>
2025-07-16 22:42:25 -0300

    Implementación al parecer correcta de Bellman-Ford, con chequeo de ciclos negativos, falta más testeo

--- graphs/src/main/java/BellmanFord.java
+++ graphs/src/main/java/BellmanFord.java
@@ -23,9 +23,20 @@ public class BellmanFord {
         }
         distTo[s] = 0.0;
 
+        for(int i = 1; i < G.V(); i++){
+            for(int v = 0; v < G.V(); v++){
+                for(DirectedEdge e: G.adj(v))
+                    relax(e);
+            }
+        }
+
         for(int i = 0; i < G.V(); i++){
-            for(DirectedEdge e: G.adj(i))
-                relax(e);
+            for(DirectedEdge e: G.adj(i)){
+                int v = e.from;
+                int w = e.to;
+                if(distTo[w] > distTo[v] + e.weight)
+                    throw new IllegalStateException("There's a negative cycle in this graph.");
+            }   
         }
     }
 
@@ -37,13 +48,28 @@ public class BellmanFord {
             edgeTo[w] = e;
         }
     }
-
-    public String pathTo(int v){
-        if(v < 0 || v >= G.V())
-            throw new IllegalArgumentException("vertex " + v + " isn't between 0 and " + (G.V()-1));
-        String s = "";
-
-        return s;
+    
+    public static void main(String[] args){
+        WeightedListIntDigraph g1 = new WeightedListIntDigraph(4);
+        DirectedEdge ed1 = new DirectedEdge(0, 1, 3);
+        DirectedEdge ed2 = new DirectedEdge(0, 2, 5);
+        DirectedEdge ed3 = new DirectedEdge(2, 3, 6);
+        DirectedEdge ed4 = new DirectedEdge(3, 2, -3);
+        g1.addEdge(ed1);
+        g1.addEdge(ed2);
+        g1.addEdge(ed3);
+        g1.addEdge(ed4);
+        BellmanFord b1 = new BellmanFord(g1, 0);
+        
+        WeightedListIntDigraph g = new WeightedListIntDigraph(4);
+        DirectedEdge e1 = new DirectedEdge(0, 1, 3);
+        DirectedEdge e2 = new DirectedEdge(0, 2, 5);
+        DirectedEdge e3 = new DirectedEdge(2, 3, 3);
+        DirectedEdge e4 = new DirectedEdge(3, 2, -6);
+        g.addEdge(e1);
+        g.addEdge(e2);
+        g.addEdge(e3);
+        g.addEdge(e4);
+        BellmanFord b = new BellmanFord(g, 0);
     }
- 
 }
